f3003534220a7c146361d8827fd8c0a24f09ea6b
efkidgraphix <162009334+efkidgraphix@users.noreply.github.com>
2024-05-05 07:49:38 +0200

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -20,7 +20,7 @@ Yo, my prefix is [ 𓆩/𓆪 ]\n
 ➥ ✓help [number of page] -> see commands
 ➥ ✓sim [message] -> talk to bot
 ➥ ✓callad [message] -> report any problem encountered
-➥ ✓help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/swordigo.swordslush`,
+➥ ✓help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/efkidtrapgamer`,
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/M4luPbE.gif")
  });
  }
