ba6cd47f7b47fa47b7404f3fc0032abe662bfef4
efkidgraphix <162009334+efkidgraphix@users.noreply.github.com>
2024-05-06 23:25:00 +0200

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -21,7 +21,7 @@ Yo, my prefix is [ 𓆩/𓆪 ]\n
 ➥ ✓sim [message] -> talk to bot
 ➥ ✓callad [message] -> report any problem encountered
 ➥ ✓help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/efkidtrapgamer`,
- attachment: await global.utils.getStreamFromURL("https://makeagif.com/i/4219ny")
+ attachment: await global.utils.getStreamFromURL("https://makeagif.com/i/4219ny.gif")
  });
  }
  }
